7d49596c18368eb2e387fab32d47bfb8416a9293
Immagorst <nmh31052005179@gmail.com>
2024-12-17 20:40:56 +0700

    Update LoginRegister.js

--- src/pages/LoginRegister.js
+++ src/pages/LoginRegister.js
@@ -58,7 +58,7 @@ const LoginRegister = ({ onLogin }) => {
     const handleRegisterSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch("http://localhost:5000/api/auth/register", {
+            const response = await fetch("https://beserver123.onrender.com/api/auth/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
