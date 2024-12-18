4f9b07bff770f2d5bd2f19e0aec698addd2d619f
Immagorst <nmh31052005179@gmail.com>
2024-12-18 18:19:54 +0700

    Update LoginRegister.js

--- src/pages/LoginRegister.js
+++ src/pages/LoginRegister.js
@@ -58,7 +58,7 @@ const LoginRegister = ({ onLogin }) => {
     const handleRegisterSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch("https://web-full-stack-3.onrender.comapi/auth/register", {
+            const response = await fetch("https://web-full-stack-3.onrender.com/api/auth/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
